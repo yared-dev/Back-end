3bd36991109c7e310f638d6c98d7f652e93f1a62
YARED <ygalindo@villasalud.pe>
2022-05-16 11:21:15 -0500

    model

--- controllers/graficos.js
+++ controllers/graficos.js
@@ -2,14 +2,28 @@ const Graficos = require("../models/graficos");
 
 // FUNCION PARA RETORNAR EL TOTAL DE LA SUMA DE PRECIOS DEL MES
 const get_data_trabajos = async (req, res) => {
-  const { desde, hasta } = req.body;
-  const grafic = await Graficos.get_data_horario(desde, hasta);
+  let { desde, hasta } = req.body;
+  let d = formatDate(desde);
+  let h = formatDate(hasta);
+  const grafic = await Graficos.get_data_horario(d, h);
   res.json({
     ok: true,
     grafic: grafic.rows,
   });
 };
-
+const formatDate = (date) => {
+  let d = new Date(date);
+  let month = (d.getMonth() + 1).toString();
+  let day = d.getDate().toString();
+  let year = d.getFullYear();
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+  return [year, month, day].join('-');
+}
 module.exports = {
   get_data_trabajos,
 };
--- models/horario.js
+++ models/horario.js
@@ -31,7 +31,7 @@ function formatDate(date) {
   if (month.length < 2) month = "0" + month;
   if (day.length < 2) day = "0" + day;
 
-  return [day, month, year].join("-");
+  return [year, month,day].join("-");
 }
 module.exports = {
   insertHorario,
